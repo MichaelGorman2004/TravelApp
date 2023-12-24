2ee21d115cbec40e9fccfb59c2791e3fc2879323
Michael Gorman <gorman.270@osu.edu>
2023-12-24 01:46:13 -0600

    Rough draft of user schema, need to look into PFP url validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,13 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 //model to store user data, currently name is not used but can be implemented further
 const userSchema = new Schema ( {
-    name: {
+    //email but storing as _id
+    _id: {
         type: String,
+        required: true,
+        unique: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    pfpUrl: {
+        type: String,
+        required: false
+    },
+    distanceTraveled: {
+        type: Number,
         required: true
     },
-    message: {
-        type: String,
+    townsVisited: {
+        type: Number,
         required: true
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        }
     }
 }, {timestamps: true});
 
